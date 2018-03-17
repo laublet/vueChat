8197c490132a45d9c118545ab7d5a03aa498b382
HanP77 <loic.aublet@gmail.com>
2018-03-17 18:46:35 +0100

    crossing fingers

--- back/modules/checkToken/index.js
+++ back/modules/checkToken/index.js
@@ -1,23 +1,31 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
-import bodyParser from 'body-parser'
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bodyParser from "body-parser";
 
 let app = express();
 let checkToken = (req, res, next) => {
-// console.log(req.headers);
-  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === process.env.AUTHBEARER) {
-    jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRETKEY, function (err, decode) {
-      if (err) res.status(500).json({success: false, message: err})
+  // console.log(req.headers);
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === process.env.AUTHBEARER
+  ) {
+    jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      process.env.SECRETKEY,
+      function(err, decode) {
+        if (err) res.status(500).json({ success: false, message: err });
         else {
-          console.log('decode HERE')
-          console.log(decode)
-          req.decode = decode
-          next()
+          // console.log('decode HERE')
+          // console.log(decode)
+          req.decode = decode;
+          next();
         }
-      });
+      }
+    );
   } else {
-    res.status(403).json({success: false, message: 'You shall not pass !'})
+    res.status(403).json({ success: false, message: "You shall not pass !" });
   }
 };
 
