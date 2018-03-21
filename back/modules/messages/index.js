a958a185b6698d9a9f9d84e0d9ffc1babcb37515
HanP77 <loic.aublet@gmail.com>
2018-03-21 12:51:18 +0100

    test

--- back/modules/messages/index.js
+++ back/modules/messages/index.js
@@ -1,35 +1,46 @@
-import express from 'express'
-import connection from '../connection/index'
-import User from './../../models/User'
-import Message from './../../models/Message'
+import express from "express";
+import connection from "../connection/index";
+import User from "./../../models/User";
+import Message from "./../../models/Message";
 let messages = express.Router();
 
-
-messages.post('/' , (req, res) => {
-	User.find({username: req.body.username},(err, usersMessages) => {
-		if (err) { res.status(500).json({success: false, message: err.message})
-	} else {
-		let newMessage = new Message(req.body)
-		newMessage.senderId = req.decode.username;
-		newMessage.save(function (err, naming) {
-			if(err) {
-				res.status(400).json({sucees: false, message: err.message})
-			} else {
-				res.status(200).json({success: true, message: 'Message posted'})
-			}
-		})
-	}
-})
+messages.post("/", (req, res) => {
+	User.find({ username: req.body.receiverId }, (err, usersMessages) => {
+		if (err) {
+			res.status(500).json({ success: false, message: err.message });
+		} else {
+			let newMessage = new Message(req.body);
+			newMessage.senderId = req.decode.username;
+			newMessage.save(function(err, naming) {
+				if (err) {
+					res
+						.status(400)
+						.json({ sucees: false, message: err.message });
+				} else {
+					res
+						.status(200)
+						.json({ success: true, message: "Message posted" });
+				}
+			});
+		}
+	});
 });
 
-messages.get('/' , (req, res) => {
-	Message.find({receiverId: req.decode.username},(err, usersMessages) => {
-			// Message.findAndUpdate({receiverId: req.decode.username}, {read: true}, {new: true} ,(err, usersMessages) => {
-		if (err) { res.status(500).json({success: false, message: err.message})
-			} else {
-			 res.status(200).json({success: true, message: 'Here are your message!', content: usersMessages});
-			}
-	})
+messages.get("/", (req, res) => {
+	Message.find({ receiverId: req.decode.username }, (err, usersMessages) => {
+		// Message.findAndUpdate({receiverId: req.decode.username}, {read: true}, {new: true} ,(err, usersMessages) => {
+		if (err) {
+			res.status(500).json({ success: false, message: err.message });
+		} else {
+			res
+				.status(200)
+				.json({
+					success: true,
+					message: "Here are your message!",
+					content: usersMessages
+				});
+		}
+	});
 });
 
-export default messages ;
+export default messages;
