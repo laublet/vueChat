edd0c56352295d86062a0960b7c603c12bb74af4
HanP77 <loic.aublet@gmail.com>
2018-05-02 23:47:35 +0200

    test

--- eslintTest/app.js
+++ eslintTest/app.js
@@ -1,9 +1,9 @@
-const bar = 1
-const foo = bar + 20
+const bar = 1;
+const foo = bar + 20;
 
 const obj = {
-  name: 'test',
-  age: 26,
-}
+	name: "test",
+	age: 26,
+};
 
-document.write(foo)
+document.write(foo);
