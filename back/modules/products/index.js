329bfe8ca7c0fc19b88d7ad8534b1aeb8ebd6ca5
HanP77 <loic.aublet@gmail.com>
2018-05-03 00:04:09 +0200

    pbl fixed

--- back/modules/products/index.js
+++ back/modules/products/index.js
@@ -7,60 +7,61 @@ import Product from "./../../models/Product";
 let products = express.Router();
 
 const storage = multer.diskStorage({
-	destination: "./public/productImages/",
-	filename: function(req, file, cb) {
-		cb(null, file.fieldname + "-" + Date.now());
-	}
+  destination: "./public/productImages/",
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  }
 });
 
 const upload = multer({
-	storage: storage,
-	limits:{fileSize: 9000000}
-	// fileFilter: function(req, file, cb){
-	//   checkFileType(file, cb);
-	// }
+  storage: storage,
+  limits: { fileSize: 9000000 }
+  // fileFilter: function(req, file, cb){
+  //   checkFileType(file, cb);
+  // }
 });
 
 products.get("/", (req, res) => {
-	Product.find({}, (err, productList) => {
-		if (err) res.status(500).json({ success: false, message: err.message });
-		else
-			res.status(200).json({
-				success: true,
-				message: "Here is the list of products!",
-				content: productList
-			});
-	});
+  Product.find({}, (err, productList) => {
+    if (err) res.status(500).json({ success: false, message: err.message });
+    else
+      res.status(200).json({
+        success: true,
+        message: "Here is the list of products!",
+        content: productList
+      });
+  });
 });
 
 products.get("/:id", (req, res) => {
-	Product.findOne({ _id: req.params.id }, (err, uniqueProduct) => {
-		if (err) res.status(500).json({ success: false, message: err.message });
-		else
-			res.status(200).json({
-				success: true,
-				message: "Here is your product!",
-				content: uniqueProduct
-			});
-	});
+  Product.findOne({ _id: req.params.id }, (err, uniqueProduct) => {
+    if (err) res.status(500).json({ success: false, message: err.message });
+    else
+      res.status(200).json({
+        success: true,
+        message: "Here is your product!",
+        content: uniqueProduct
+      });
+  });
 });
 
 products.post("/", upload.single("picture"), (req, res) => {
-	let newProduct = new Product(JSON.parse(req.body.product));
-	if (req.file) newProduct.pictures = req.file.filename;
-	newProduct.userId = req.decode.id;
-	newProduct.username = req.decode.username;
-	newProduct.save(function(err, product) {
-		if (err) {
-			res.status(400).json({ success: false, message: err.message });
-		} else {
-			res.status(200).json({
-				success: true,
-				message: "Product created",
-				content: product
-			});
-		}
-	});
+  let newProduct = new Product(JSON.parse(req.body.product));
+  console.log("ICI", req.file);
+  if (req.file) newProduct.pictures = req.file.filename;
+  newProduct.userId = req.decode.id;
+  newProduct.username = req.decode.username;
+  newProduct.save((err, product) => {
+    if (err) {
+      res.status(400).json({ success: false, message: err.message });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Product created",
+        content: product
+      });
+    }
+  });
 });
 
 export default products;
